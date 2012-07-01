cf67c5a28467365085e880e3d59e1d5af01ffc26
J. Melzer <ju.melzer@gmail.com>
2012-07-01 11:56:40 +0200

    add more fields

--- src/main/java/com/jmelzer/webapp/page/ShowIssuePage.java
+++ src/main/java/com/jmelzer/webapp/page/ShowIssuePage.java
@@ -16,6 +16,8 @@ import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 public class ShowIssuePage extends MainPage {
@@ -35,9 +37,17 @@ public class ShowIssuePage extends MainPage {
             //todo
         }
         add(new Label("projectname", issue.getProject().getName()));
-        add(new Label("issuesummary", issue.getSummary()));
-        Link link = new BookmarkablePageLink("self", ShowIssuePage.class, parameters);
+        Link link = new BookmarkablePageLink("self2", ShowIssuePage.class, parameters);
+        link.add(new Label("issuesummary", issue.getSummary()));
+        add(link);
+        link = new BookmarkablePageLink("self", ShowIssuePage.class, parameters);
         add(link);
         link.add(new Label("name", issueName));
+
+        add(new Label("issuetypelabel", new StringResourceModel("issuetype", new Model(""))));
+        add(new Label("issuetype", issue.getType().getType()));
+
+        add(new Label("prioritylabel", new StringResourceModel("priority", new Model(""))));
+        add(new Label("priority", issue.getPriority().getName()));
     }
 }
